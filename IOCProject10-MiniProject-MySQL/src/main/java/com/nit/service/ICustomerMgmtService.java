886615a73d6056c87a88b9468957c531878b4a55
arvind01A <arvind.kumar208414@gmail.com>
2024-03-22 00:33:37 +0530

    Here use of mysql database develop in miniproject

---
+++ IOCProject10-MiniProject-MySQL/src/main/java/com/nit/service/ICustomerMgmtService.java
@@ -0,0 +1,8 @@
+//ICustomerMgmtService.java
+package com.nit.service;
+
+import com.nit.model.Customer;
+
+public interface ICustomerMgmtService {
+	public String registrerCustomer(Customer customer) throws Exception;
+}
